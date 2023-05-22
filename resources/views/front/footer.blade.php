fb01184b94d9735655e645bce7ea13beacf296a5
Albert Muhatia <albertmuhatia@gmail.com>
2023-05-22 15:37:33 +0300

    Update

---
+++ resources/views/front/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="position-relative pt-140">
+    <div class="footer-bg" data-background="{{asset('theme/assets/img/bg/footer/img-1.jpg')}}">
+    </div>
+    <div class="footer-area">
+       <div class="main-footer-top">
+          <div class="container">
+             <div class="row">
+                <div class="col-12">
+                   <div class="footer-contact text-center">
+                      <div class="footer-contact-text">
+                         <h2 class="footer-contact-title bdFadeUp">If you'd like to <a class="mark-text"
+                               href="contact.html">talk to us</a>
+                            about a project,
+                            we would love to hear from you.</h2>
+                      </div>
+                      <div class="footer-logo-wrapper">
+                         <a href="index.html" class="fm-footer-logo">
+                            <img src="{{asset('theme/assets/img/logo/logo.png')}}" alt="img not found">
+                         </a>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div class="footer-bottom">
+          <div class="container">
+             <div class="footer-bottom-wrapper">
+                <div class="footer-bottom-inner">
+                   <div class="footer-copyright ">
+                      <p>© 2023<a href="index.html"> Faime </a> All Rights Reserved.</p>
+                   </div>
+
+
+                   <div class="footer-rules ">
+                      <a href="#">Privacy Policy</a>
+                      <a href="#">Terms & Condition</a>
+                   </div>
+                   <div class="footer-social ">
+                      <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                      <a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                      <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                      <a href="https://www.pinterest.com/" target="_blank"><i
+                            class="fa-brands fa-pinterest-p"></i></a>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </footer>
---
+++ resources/views/front/header.blade.php
@@ -0,0 +1,110 @@
+<header id="header-sticky" class="fm-header-area  header-spc transparent-header">
+    <div class="container">
+       <div class="row align-items-center justify-content-lg-between">
+          <div class="header-wrapper">
+             <div class="fm-header-logo-wrapper">
+                <a href="index.html" class="fm-header-logo">
+                   <img src="{{asset('theme/assets/img/logo/logo.png')}}" alt="img not found">
+                </a>
+                <a href="#0" class="fm-header-bar-1">
+                   <span class="fm-header-bar-line fm-header-bar-line-1"></span>
+                   <span class="fm-header-bar-line fm-header-bar-line-2"></span>
+                   <span class="fm-header-bar-line fm-header-bar-line-3"></span>
+                </a>
+             </div>
+             <div class="header-menu-spc d-none d-xl-block">
+                <div class="fm-header-nav">
+
+                   <nav id="mobile-menu">
+                      <ul>
+                         <li class="active has-dropdown">
+                            <a href="index.html">Home</a>
+                            <ul class="submenu">
+                               <li><a href="index.html">Movie Director</a></li>
+                               <li><a href="index-2.html">Filming Works</a></li>
+                               <li><a href="movie-film.html">Movie and Film</a></li>
+                               <li><a href="filming-history.html">Filming History</a></li>
+                               <li><a href="updated-movie.html">Movie Features</a></li>
+                            </ul>
+                         </li>
+
+
+                         <li class="has-dropdown"><a href="#">Movie</a>
+                            <ul class="submenu">
+                               <li><a href="movie.html">Movie</a></li>
+                               <li><a href="movie-details.html">Movie Details</a></li>
+                            </ul>
+                         </li>
+
+                         <li class="has-dropdown"><a href="#">Portfolio</a>
+                            <ul class="submenu">
+                               <li><a href="portfolio-masonary.html">Masonary</a></li>
+                               <li><a href="portfolio-slider.html">Slider</a></li>
+                               <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                            </ul>
+                         </li>
+
+                         <li class="has-dropdown has-mega-menu">
+                            <a href="#">Pages</a>
+                            <ul class="mega-menu">
+                               <li><a href="javasript:void(0);" class="mega-menu-title">Page Layout 1</a>
+                                  <ul>
+                                     <li> <a href="about.html">About us</a></li>
+                                     <li><a href="service.html">Service</a></li>
+                                     <li><a href="service-details.html">Service Details</a></li>
+                                     <li><a href="team.html">Team</a></li>
+                                     <li><a href="team-details.html">Team Details</a></li>
+                                  </ul>
+                               </li>
+                               <li><a href="javasript:void(0);" class="mega-menu-title">Page Layout 2</a>
+                                  <ul>
+                                     <li><a href="faq.html">FAQ's</a></li>
+                                     <li><a href="pricing.html">Pricing</a></li>
+                                     <li><a href="cart.html">Cart</a></li>
+                                     <li><a href="contact.html">Contact Us</a></li>
+                                     <li><a href="404.html">404</a></li>
+                                  </ul>
+                               </li>
+                               <li><a href="javasript:void(0);" class="mega-menu-title">Page Layout 3</a>
+                                  <ul>
+                                     <li><a href="shop-sidebar.html">Shop Sidebar</a></li>
+                                     <li><a href="shop-details.html">Shop Details</a></li>
+                                     <li><a href="shop-4.html">Shop 4 Column</a></li>
+                                     <li><a href="blog.html">Blog Standard</a></li>
+                                     <li><a href="blog-details.html">Blog Details</a></li>
+                                  </ul>
+                               </li>
+                            </ul>
+                         </li>
+
+                         <li class="has-dropdown"><a href="#"> Blog</a>
+                            <ul class="submenu">
+                               <li><a href="blog-wide.html">Blog Wide</a></li>
+                               <li><a href="blog.html">Blog Standard </a></li>
+                               <li><a href="blog-details.html">Blog Details</a></li>
+                            </ul>
+                         </li>
+
+
+                         <li class="has-dropdown"><a href="#">Contact</a>
+                            <ul class="submenu">
+                               <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                         </li>
+                      </ul>
+                   </nav>
+
+                </div>
+             </div>
+             <div class="fm-header-right d-none d-xl-block">
+                <a href="contact.html" class="hero-btn">Let's Chat
+                   <span>
+                      <i class="fa-regular fa-arrow-right"></i>
+                   </span>
+                </a>
+             </div>
+          </div>
+
+       </div>
+    </div>
+ </header>
